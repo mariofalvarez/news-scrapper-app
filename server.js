2b83427e8637d1d3c15c96c951063921408b109d
mariofalvarez <mariofalvarezjr@gmail.com>
2020-02-11 12:38:43 -0800

    setup boiler plate and server.js

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+const connection = mongoose.connection
+const logger = require("morgan")
+const PORT = process.env.PORT || 3000
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines"
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+})
+
+connection.on("error", console.error.bind(console, "connection error:"))
+connection.once("open", function() {
+  console.log("connected to db instance")
+})
+
+app.use(logger("dev"))
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static("./views"))
+
+const apiRoutes = require("./routes/api-routes")
+app.use("/api", apiRoutes)
+
+const htmlRoutes = require("./routes/html-routes")
+app.use("/", htmlRoutes)
+
+app.listen(PORT, () => {
+  console.log(`listening at: http://localhost:${PORT}`)
+})
